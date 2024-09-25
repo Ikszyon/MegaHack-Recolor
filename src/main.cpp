ab57baacf61b719922f5470ee4157dff7ecf8362
Ikszyon <75569094+Ikszyon@users.noreply.github.com>
2024-09-25 16:41:01 +0200

    Still alive

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 
-#include "mhinfo.hpp"
-#include "mhmenu.hpp"
-#include "gui.hpp"
-#include "colorfade.hpp"
-#include "animationhandler.hpp"
-#include "savedata.hpp"
 #include "manager.hpp"
 
 $on_mod(Loaded) {
